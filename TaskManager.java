9c3e42a733bd0af7eeb4a5b9d5958111b473afcc
unknown <suhao0721@gmail.com>
2024-03-12 01:39:32 +0800

    Initial commit

---
+++ TaskManager.java
@@ -0,0 +1,29 @@
+public class TaskManager {
+    
+    public static void addTask(){
+
+    }
+
+    public static void deleteTask(){
+
+    }
+
+    public static void updateTask(){
+
+    }
+
+    public static void filterTask(){
+
+    }
+
+    public static void remindTask(){
+
+    }
+
+    public static void markTask(){
+
+    }
+
+    
+
+}
