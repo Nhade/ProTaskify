6849ab2a1ed06911a5811cc948660b1d2f000d21
WeiChanHo <weichan1117@gmail.com>
2024-03-13 01:55:25 +0800

    feat: Added four methods:`addTask`,`deleteTask`,`updateTask`,`markTask`
    Do notice that the `addTask` method has a unsolved problem that how to deal with the
    repeated tasknames, and the functionality `updateTask` method is currently ambiguous.

--- TaskManager.java
+++ TaskManager.java
@@ -1,34 +1,48 @@
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class TaskManager {
-    public TaskManager(){
+    public TaskManager() {
         FileManager FileIO = new FileManager();
         ArrayList<Task> taskList = FileIO.loadTasks("testSave.ser");
     }
-    public static void addTask(){
+
+    public static void addTask(ArrayList<Task> taskList, String name, Priority priority, LocalDateTime time) {
+        Task addedTask = new Task(name, priority, time);
+        taskList.add(addedTask);
+    }
+
+    public static void deleteTask(ArrayList<Task> taskList, String deletedName) {
+        for (Task task : taskList) {
+            if (task.taskName.equals(deletedName)) {
+                taskList.remove(task);
+            }
+        }
+    }
+
+    public static void updateTask(ArrayList<Task> taskList, String targetName, Priority priority, LocalDateTime time) {
+        for (Task task : taskList) {
+            if (task.taskName.equals(targetName)) {
+                task.priority = priority;
+                task.deadline = time;
+            }
+        }
+    }
+
+    public static void filterTask() {
 
     }
 
-    public static void deleteTask(){
+    public static void remindTask() {
 
     }
 
-    public static void updateTask(){
-
+    public static void markTask(ArrayList<Task> taskList,String targetName) {
+        for (Task task : taskList) {
+            if (task.taskName.equals(targetName)) {
+                task.isDone = true;
+            }
+        }
     }
-
-    public static void filterTask(){
-
-    }
-
-    public static void remindTask(){
-
-    }
-
-    public static void markTask(){
-
-    }
-
-    
 
 }
